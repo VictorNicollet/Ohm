5e995930daee88a1550e3e5711e0e8bf52741d36
Victor Nicollet <victor-github@nicollet.net>
2012-05-14 14:09:29 +0200

    Missing file

---
+++ src/action_Endpoint.ml
@@ -0,0 +1,20 @@
+(* Ohm is © 2012 Victor Nicollet *)
+
+open Util
+open BatPervasives
+
+open Action_Common
+
+module Server = Action_Server
+module Args   = Action_Args
+
+type ('server,'args) endpoint = 'server -> 'args -> string
+
+let url endpoint s a = endpoint s a 
+
+let endpoint_of_controller (server,prefix,parse) =
+  let prefix = path_clean (lowercase prefix) in
+  fun s a ->  
+    String.concat "/" 
+      ( Server.server_root server s :: prefix :: 
+	  List.map (Netencoding.Url.encode ~plus:false) (Args.generate parse a))
