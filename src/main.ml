b1e5d0b74e5ce9e80f6cb85090f6e963c76b532f
Victor Nicollet <victor@nicollet.net>
2012-05-14 18:57:47 +0200

    Connect Async with main module

--- src/main.ml
+++ src/main.ml
@@ -4,7 +4,7 @@ module Make =
   functor (Reset:Reset.RESET) ->
 struct
 
-  let run () = 
+  let run async = 
 
     let web_loop () = 
       if not (Reset.resetting ()) then 
@@ -15,12 +15,26 @@ struct
       let last_compact = ref 0. in
       if not (Reset.resetting ()) then 
 	let rec loop_check () = 
+
+	  (* Stop looping if a reset is requested. *)
 	  Reset.check () ;
+
+	  (* Compact databases every hour *)
 	  let now = Unix.gettimeofday () in
 	  if now > !last_compact +. 3600.0 then 
 	    ( last_compact := now ; CouchDB.compact ()) ;
-	  Unix.sleep 2000 ;
+
+	  (* Perform one run of async processing. *)
+	  let wait = 
+	    match async with None -> true | Some async -> 
+	      not (async ())
+	  in
+
+	  (* If the run finished all tasks, sleep for one second. *)
+	  if wait then Unix.sleep 1 ;
+	      
 	  loop_check () ;
+
 	in 
 	loop_check () 
     in
@@ -32,3 +46,4 @@ struct
       | `Reset -> Run.eval (new CouchDB.init_ctx) (Reset.run ())
 
 end
+
