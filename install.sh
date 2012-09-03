678433c5d560a8391821b8063eb331f1885eab23
Victor Nicollet <victor@nicollet.net>
2012-09-03 23:40:55 +0200

    Clean up after install

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ocamlc run.ml -o /usr/local/bin/ohm && chmod a+x /usr/local/bin/ohm && echo "Ohm installed !" && echo "Run 'ohm init <directory>' to create a new project"
+ocamlc run.ml -o /usr/local/bin/ohm && chmod a+x /usr/local/bin/ohm && echo "Ohm installed !" && echo "Run 'ohm init <directory>' to create a new project" && rm run.cmo run.cmi 
