a22e04e7f0d6e994061bb01536163999cce22746
Victor Nicollet <victor@nicollet.net>
2013-01-11 22:09:10 +0100

    Model generation

--- tool/snip.ml
+++ tool/snip.ml
@@ -22,14 +22,55 @@ let put list = List.iter (fun (file,contents) ->
     print_endline (">> " ^ file) 
 ) list
 
+(* Actual generation ---------------------------------------------------------------------- *)
+
 let id name = put [
   ml  name, "include Ohm.Id.Phantom\n" ;
   mli name, "include Ohm.Id.PHANTOM\n" ;
 ]
 
+let model name = 
+  let short = BatString.lchop name in 
+  id ("I" ^ short) ; 
+  put [
+
+    ml name, Printf.sprintf "open Ohm
+open Ohm.Universal
+
+module Data = struct
+  module T = struct
+    type json t = {
+    }
+  end
+  include T
+  include Fmt.Extend(T)
+end
+
+include CouchDB.Convenience.Table
+  (struct let db = O.db %S end)(I%s)(Data)
+" 
+      (String.lowercase short) (* DB name *)
+      short ; 
+
+    mli name, Printf.sprintf "module Data : sig 
+  type t = {
+  }
+end
+
+module Tbl : Ohm.CouchDB.TABLE 
+  with type elt = Data.t 
+  and  type id  = I%s.t
+
+module Design : Ohm.CouchDB.DESIGN
+"
+      short
+]
+
+(* Entry pointration ---------------------------------------------------------------------- *)
 
 let one = function
   | name when name -- "I[A-Za-z0-9]*" -> id name
+  | name when name -- "M[A-Za-z0-9]*" -> model name 
   | name -> print_endline (Printf.sprintf "Don't know what to do with %S" name)
 
 let make args = List.iter one args
